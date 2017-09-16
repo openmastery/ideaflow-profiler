dfb8c13d0a77adb8427d34c1eec0564d48654e33
Janelle Klein <janelle@openmastery.org>
2017-09-16 16:12:54 -0500

    [Bugfix] [Hack] Keep most recently used API key as state for other calls

--- src/services/task/task.service.ts
+++ src/services/task/task.service.ts
@@ -12,13 +12,14 @@ import {User} from "../../app/models/user";
 @Injectable()
 export class TaskService {
   private apiUrl = 'http://ideaflowdx.openmastery.org';
+  private mostRecentApiKey = Constants.X_API_Key;
 
   constructor(private http: Http ){
 
   }
 
   updateEvent(eventPath, jsonPatch) {
-    let headers = new Headers({'X-API-Key': Constants.X_API_Key, 'Content-Type': 'application/json'});
+    let headers = new Headers({'X-API-Key': this.mostRecentApiKey, 'Content-Type': 'application/json'});
     let options = new RequestOptions({headers: headers});
 
     let url = this.apiUrl + "/ideaflow" + eventPath;
@@ -29,8 +30,13 @@ export class TaskService {
   }
 
   getTasksForUser (user: User): Observable<Task[]> {
-    const headers = new Headers({ 'X-API-Key': Constants.X_API_Key });
+    //TODO fix this ugly side-effect hack
+    this.mostRecentApiKey = user.apiKey;
+
+    const headers = new Headers({ 'X-API-Key': this.mostRecentApiKey });
     const options = new RequestOptions({headers: headers});
+
+
 
     return this.http
       .get(this.apiUrl + '/ideaflow/task/user/'+user.id, options)
@@ -50,7 +56,7 @@ export class TaskService {
 
   getTaskFullDetail (taskId): Observable<TaskFullDetail> {
     let projectParam = (taskId) ? taskId:'';
-    let headers = new Headers({ 'X-API-Key': Constants.X_API_Key });
+    let headers = new Headers({ 'X-API-Key': this.mostRecentApiKey });
     let options = new RequestOptions({headers: headers});
 
     return this.http
